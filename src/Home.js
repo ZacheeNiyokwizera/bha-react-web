6f23d5b8349740a67736392ac000bd8edae18a0d
Zack <zack@labs.ws>
2021-11-18 17:59:14 +0200

    fixed a bug

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import homePic from "./home.jpg";
 
 function Home() {
   return (
-    <div>
-      <h1>This is Home </h1>
-    </div>
+
+    <section id="hero">
+        <header class="new-header">
+            <div class="img-wrapper">
+                <img src={homePic} />
+            </div>
+            <div class="banner">
+                <h1>BHA</h1>
+                <h1>Bridge of Hope Association</h1>
+                <p>It's morning again</p>
+
+                <a href="about.html" class="cta">Discover</a>
+            </div>
+        </header>
+    </section>
   );
 }
 
